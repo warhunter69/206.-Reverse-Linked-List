b3c3ac195634bda76a06b6026dca1e395cf72ba4
ahmedmahmoud <34657198+warhunter69@users.noreply.github.com>
2022-03-31 09:24:59 +0200

    Create 206. Reverse Linked List.js

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,15 @@
+var reverseList = function(head) {
+    let prev = null;
+    let next = null;
+    let node = head;
+    while(node){
+        next = node.next
+        node.next = prev
+        prev = node;
+        node = next;
+        
+        
+    }
+    return prev
+
+};
